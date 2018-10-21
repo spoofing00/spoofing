f01618de6e45d2085544be034d623fa59c11ed0c
spoofing0 <44108100+spoofing0@users.noreply.github.com>
2018-10-22 01:38:08 +0300

    Update Deneme.py

--- Deneme.py
+++ Deneme.py
@@ -5,8 +5,8 @@ import random
 import threading
 import sys
 
-ip = str(input("Hedef/IP : "))
-port = int(input("Port : "))
-thread_num = int(input("Threads : "))
-out_file = str(input("Proxy [proxy.txt] : "))
-print ("Number Of Proxies : %s" %(len(open(out_file).readlines())))
+ip = str(input("[+] Hedef/IP : "))
+port = int(input("[+] Port : "))
+thread_num = int(input("[+] Threads : "))
+out_file = str(input("[+] Proxy [proxy.txt] : "))
+print ("[+] Number Of Proxies : %s" %(len(open(out_file).readlines())))
